2bb7030a52a316272d41760d05c8082785350119
Maida Eyewear <maidaeyewear@gmail.com>
2024-02-02 15:17:00 +0700

    response

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -41,10 +41,8 @@ export async function POST(request, {params}){
                 body: JSON.stringify({ID,post_author,post_title,post_content,post_date,post_date_gmt,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count,filter,post_meta,post_thumbnail,post_permalink,taxonomies}),
             });
         }
-        await sendData();
-        //Response
-        return NextResponse.json({message: "Data Terkirim!",post_permalink},{status:201});  
+        await sendData(); 
     }
-
-     
+    //Response
+    return NextResponse.json({message: "Data Terkirim!",post_permalink},{status:201}); 
 }
