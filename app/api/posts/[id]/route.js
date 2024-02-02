d1d2a97745d30006d80ac90f37817b63bd7cded2
Maida Eyewear <maidaeyewear@gmail.com>
2024-02-02 15:11:33 +0700

    update titel

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -30,18 +30,21 @@ export async function POST(request, {params}){
     const post_permalink = "https:\/\/"+`${url}`+"\/?p="+`${ID}`;
 
     //Send Data To Another Web
-    const sendData = async() => {
-        await fetch("https://ujicoba2.polressemarangkab.com/?wpwhpro_action=main_8829&wpwhpro_api_key=r79cwjl1up40xxc45qrdjonbuzuletgz38bgpqgdkpw5yvx1jkfbquzsnehrbcro&action=create_post",{
-        // await fetch("https://webhook.site/27d6ef8d-6b16-4698-a33e-cf7b4b64b38a",{
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify({ID,post_author,post_title,post_content,post_date,post_date_gmt,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count,filter,post_meta,post_thumbnail,post_permalink,taxonomies}),
-        });
+    if(post_title != "Auto Draft"){
+        const sendData = async() => {
+            await fetch("https://ujicoba2.polressemarangkab.com/?wpwhpro_action=main_8829&wpwhpro_api_key=r79cwjl1up40xxc45qrdjonbuzuletgz38bgpqgdkpw5yvx1jkfbquzsnehrbcro&action=create_post",{
+            // await fetch("https://webhook.site/27d6ef8d-6b16-4698-a33e-cf7b4b64b38a",{
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({ID,post_author,post_title,post_content,post_date,post_date_gmt,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count,filter,post_meta,post_thumbnail,post_permalink,taxonomies}),
+            });
+        }
+        await sendData();
+        //Response
+        return NextResponse.json({message: "Data Terkirim!",post_permalink},{status:201});  
     }
-    await sendData();
 
-    //Response
-    return NextResponse.json({message: "Data Terkirim!",post_permalink},{status:201});   
+     
 }
